870baabbd4c72d7cd2fcd47df7dbd68143bc3080
zgwaiwyd <99521061+zgwaiwyd@users.noreply.github.com>
2022-02-19 22:49:37 +0800

    Add files via upload

---
+++ game.h
@@ -0,0 +1,36 @@
+#pragma once
+
+
+//头文件的包含
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+//符号的定义
+#define ROW 10
+#define COL 10
+
+//函数的声明
+
+//初始化棋盘的
+void InitBoard(char board[ROW][COL], int row, int col);
+
+//打印棋盘的函数
+void DisplayBoard(char board[ROW][COL], int row, int col);
+
+//玩家下棋
+void PlayerMoveA(char board[ROW][COL], int row, int col);
+
+//电脑下棋
+void PlayerMoveB(char board[ROW][COL], int row, int col);
+
+//
+//1. 玩家赢了 - *
+//2. 电脑赢了 - #
+//3. 平局 - Q
+//4. 游戏继续 - C
+
+//判断游戏是否有输赢
+char IsWin(char board[ROW][COL], int row, int col);
+
